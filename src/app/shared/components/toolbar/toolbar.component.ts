ec2e2c35926a8ae4f299cd15c2ee03e42b70a923
Cristiano10 <ccristiandarioromero@gmail.com>
2021-06-13 06:53:49 -0300

    moldeando

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
-  public appName = 'Virtual';
+  public appName = 'Ventas online 24/7';
   constructor(public authSvc: AuthService) { }
 
   ngOnInit(): void {
